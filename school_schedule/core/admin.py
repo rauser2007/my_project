3523a73e413380b914fefe943c52c7d209150dce
Дмитро <murchyk2007@gmail.com>
2024-08-30 20:29:38 +0300

    lesson 30

--- school_schedule/core/admin.py
+++ school_schedule/core/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Subject, Teacher, Class, Student
+from .models import Subject, Teacher, Class, Student, Schedule, Grade
 
 # Register your models here.
 admin.site.register(Subject)
 admin.site.register(Teacher)
 admin.site.register(Class)
 admin.site.register(Student)
+admin.site.register(Schedule)
+admin.site.register(Grade)
--- school_schedule/core/models.py
+++ school_schedule/core/models.py
@@ -25,6 +25,27 @@ class Class(models.Model):
 class Student(models.Model):
     name = models.CharField(max_length=100)
     class_group = models.ForeignKey(Class, on_delete=models.CASCADE)
+    grades = models.ManyToManyField(Subject, through="Grade")
 
     def __str__(self):
         return self.name
+
+class Schedule(models.Model):
+    subject = models.ForeignKey('Subject', on_delete=models.CASCADE)
+    class_group = models.ForeignKey('Class', on_delete=models.CASCADE)
+    teacher = models.ForeignKey('Teacher', on_delete=models.CASCADE)
+    date = models.DateField()
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+
+    def __str__(self):
+        return f"{self.class_group} - {self.subject} ({self.date})"
+
+class Grade(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    grade = models.IntegerField()
+    date = models.DateField()
+
+    def __str__(self) -> str:
+        return f"{self.student}'s grade in {self.subject} is {self.grade}"
